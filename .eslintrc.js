f5d21b3dce5dd758594ee8a3eb1a85b7e00a18b5
Your Name <damiand@vordentech.com>
2026-06-18 21:20:14 -0300

    Update ESLint configuration and upgrade eslint-plugin-import
    - Added a rule to ignore unresolved imports for the @vercel/analytics/react package in .eslintrc.js.
    - Upgraded eslint-plugin-import version in package.json from 2.24.2 to 2.32.0.

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,6 +20,12 @@ module.exports = {
   rules: {
     'prefer-template': 0,
     'react/no-children-prop': [0],
-    'linebreak-style': 0
+    'linebreak-style': 0,
+    "import/no-unresolved": [
+      "error",
+      {
+        "ignore": ["^@vercel/analytics/react$"]
+      }
+    ]
   },
 };
